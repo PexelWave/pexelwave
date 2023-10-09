995f14d3f69aeba8dfaa28ebbc7e503b33c497b3
Perfect Nkois <perfect.coding98@gmail.com>
2023-10-09 10:00:53 +0200

    Update: Remove HeroAnimation.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,8 @@
+"use client";
 import Link from "next/link";
-import HeroAnimation from "./HeroAnimation";
+import Lottie from "lottie-react";
+import heroAimation from "@/assets/lottie/heroAnimation.json";
+import Image from "next/image";
 
 const Hero = () => (
   <header className="pt-[15vh] container bg-purple text-light-foreground lg:h-screen flex flex-col-reverse lg:flex-col">
@@ -21,7 +24,19 @@ const Hero = () => (
       </Link>
     </section>
 
-    <HeroAnimation />
+    <>
+      <Lottie
+        className="hidden md:block md:absolute w-[1080px] top-0 left-1/2 -translate-x-1/2 md:left-0 md:-translate-x-0 lg:translate-x-1/2"
+        animationData={heroAimation}
+      />
+      <Image
+        className="absolute block top-20 md:hidden left-1/2 -translate-x-1/2 max-w-screen-lg"
+        src="/heroAnimation.svg"
+        alt=""
+        width={1080}
+        height={920}
+      />
+    </>
   </header>
 );
 

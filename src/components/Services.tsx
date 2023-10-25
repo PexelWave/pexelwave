fd1ebf6330c1fe19032874f4fa38855c946338ce
Perfect Nkois <perfect.coding98@gmail.com>
2023-10-25 15:22:52 +0200

    Fix: Fetch services from Sanity in Services componet in home page

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,7 +1,9 @@
-import { servicesInfo } from "@/lib/components-data";
-import Link from "next/link";
 
-const Services = () => {
+import Link from "next/link";
+import { getServices } from "../../sanity/lib/utils";
+
+const Services = async () => {
+  const services: Service[] = await getServices();
   return (
     <section className="md:mt-24 container">
       <h2 className="large-title text-light-foreground">
@@ -9,7 +11,7 @@ const Services = () => {
       </h2>
 
       <div className="mt-6 overflow-x-scroll custom-scrollbar flex gap-6 snap-x snap-mandatory">
-        {servicesInfo.map((service) => (
+        {services.map((service) => (
           <div
             key={service.title}
             className="bg-violet-900 mb-6 rounded-lg p-6 snap-center w-full md:w-1/3 shrink-0 text-light-foreground flex flex-col justify-between"
